f5e328ac9304d1adbc21cb72e73584c883de836a
Vinicios Alves <viniciosconte045@gmail.com>
2026-05-18 14:15:50 -0300

    arrumando as turmas

--- cod_teste.py
+++ cod_teste.py
@@ -17,3 +17,5 @@ def cadatrar_aluno():
     idade = input("Digite sua idade em números  ").strip()
     print("Opções de turma: \n 1: 1° EM DS \n 2: 1° EM multimídia \n 3: 1° EM Jogos Digitais \n 4: 2° EM Multimídia \n 5: 2° EM Jogos digitais \n 6: 3° EM Jogos Digitais")
     turma = input("Digite sua turma")
+    if turma > 1 or turma < 6:
+        print("turma invalida")
