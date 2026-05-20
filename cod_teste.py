80ffb3080b89fd669580be0398e69d240cea0736
Vinicios Alves <viniciosconte045@gmail.com>
2026-05-20 13:17:47 -0300

    arrumando erros e colocando o edit em ordem ou tentando

--- cod_teste.py
+++ cod_teste.py
@@ -16,7 +16,7 @@ def cadastrar_aluno():
     nome = input("Digite seu nome  ").strip()
     idade = input("Digite sua idade em números  ").strip()
     print("Opções de turma: \n 1: 1° EM DS \n 2: 1° EM multimídia \n 3: 1° EM Jogos Digitais \n 4: 2° EM Multimídia \n 5: 2° EM Jogos digitais \n 6: 3° EM Jogos Digitais")
-    turma = {
+    turmas = {
         "1": "1° EM DS",
         "2": "1° EM Multimídia",
         "3": "1° EM Jogos Digitais",
@@ -25,10 +25,10 @@ def cadastrar_aluno():
         "6": "3° EM Jogos Digitais"
     }
     opcao_turma = input("Digite o numero da sua turma:").strip()
-    if opcao_turma not in turma:
+    if opcao_turma not in turmas:
         print("Turma inválida")
-    elif nome == "" or idade == "":
-        print("preencha todos os campos")
+    elif not nome.replace(" ", "").isalpha(): 
+        print("Nome invalido")
 
     elif not idade.isdigit() or int(idade) <= 0:
         print("idade deve ser um número valido")
@@ -36,7 +36,7 @@ def cadastrar_aluno():
     else:
         cursor.execute(
             "INSERT INTO alunos (nome, idade, turma) VALUES (%s, %s, %s)",
-            (nome, int(idade), turma[opcao_turma])
+            (nome, int(idade), turmas[opcao_turma])
         )
         conexao.commit()
         print("Aluno cadastrado")
@@ -49,23 +49,26 @@ def listar_alunos():
 
     cursor.execute("SELECT * FROM alunos")
     alunos = cursor.fetchall()
+    if len(alunos) == 0:
+        print("Nenhum aluno cadastrado")
+    
     for aluno in alunos:
         print(f"ID: {aluno[0]}, Nome: {aluno[1]}, Idade: {aluno[2]}, Turma: {aluno[3]}")
     cursor.close()
     conexao.close()
 
-#def editar_aluno():
-    # n sei pq mas deu erro então vou averiguar e arrumar dps conexao = conectar()
-    # cursor = conexao.cursor()
-    # id_aluno = input("digite o ID do aluno que você quer editar:").strip()
-    # if not id_aluno.isdigit():
-    #     print("ID deve ser um número válido")
-    # else: 
-    #     cursor.execute("SELECT * FROM alunos WHERE id_aluno = %s", (id_aluno,))
-    #     aluno = cursor.fetchone()
-    #     if aluno:
-    #         print(f"ID: {aluno[0]}, Nome: {aluno[1]}, Idade: {aluno[2]}, Turma: {aluno[3]}")
-    #     else:
-    #         print("Aluno não encontrado")
-    # cursor.close()
-    # conexao.close()
+def editar_aluno():
+    conexao = conectar()
+    cursor = conexao.cursor()
+    id_aluno = input("digite o ID do aluno que você quer editar:").strip()
+    if not id_aluno.isdigit():
+        print("ID deve ser um número válido")
+    else: 
+        cursor.execute("SELECT * FROM alunos WHERE id_aluno = %s", (id_aluno,))
+        aluno = cursor.fetchone()
+        if aluno:
+            print(f"ID: {aluno[0]}, Nome: {aluno[1]}, Idade: {aluno[2]}, Turma: {aluno[3]}")
+        else:
+            print("Aluno não encontrado")
+    cursor.close()
+    conexao.close()
