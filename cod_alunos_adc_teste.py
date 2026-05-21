bd5348fcf711458714edf69b1e6a4f663ee9b164
Vinicios Alves <viniciosconte045@gmail.com>
2026-05-21 14:37:53 -0300

    arrumando os espaçamentos par ficar mais bunitim

--- cod_alunos_adc_teste.py
+++ cod_alunos_adc_teste.py
@@ -13,11 +13,11 @@ def cadastrar_aluno():
 
     cursor = conexao.cursor()
 
-    nome = input("Digite seu nome  ").strip()
+    nome = input("Digite seu nome:  ").strip()
 
-    idade = input("Digite sua idade em números  ").strip()
+    idade = input("Digite sua idade em números:  ").strip()
 
-    print("Opções de turma: \n 1: 1° EM DS \n 2: 1° EM multimídia \n 3: 1° EM Jogos Digitais \n 4: 2° EM Multimídia \n 5: 2° EM Jogos digitais \n 6: 3° EM Jogos Digitais")
+    print("Opções de turma: \n 1: 1° EM DS \n 2: 1° EM multimídia \n 3: 1° EM Jogos Digitais \n 4: 2° EM Multimídia \n 5: 2° EM Jogos digitais \n 6: 3° EM Jogos Digitais \n")
     turmas = {
 
         "1": "1° EM DS",
@@ -27,16 +27,16 @@ def cadastrar_aluno():
         "5": "2° EM Jogos Digitais",
         "6": "3° EM Jogos Digitais"
     }
-    opcao_turma = input("Digite o numero da sua turma:").strip()
+    opcao_turma = input("Digite o numero da sua turma:  ").strip()
 
     if opcao_turma not in turmas:
-        print("Turma inválida")
+        print("Turma inválida\n")
 
     elif not nome.replace(" ", "").isalpha(): 
-        print("Nome invalido")
+        print("Nome invalido\n")
 
     elif not idade.isdigit() or int(idade) <= 0:
-        print("idade deve ser um número valido")
+        print("idade deve ser um número valido\n")
 
     else:
         cursor.execute(
@@ -46,7 +46,7 @@ def cadastrar_aluno():
 
         conexao.commit()
 
-        print("Aluno cadastrado")
+        print("Aluno cadastrado\n")
 
     cursor.close()
 
@@ -62,7 +62,7 @@ def listar_alunos():
     alunos = cursor.fetchall()
 
     if len(alunos) == 0:
-        print("Nenhum aluno cadastrado")
+        print("Nenhum aluno cadastrado\n")
     
     for aluno in alunos:
         print(f"ID: {aluno[0]}, Nome: {aluno[1]}, Idade: {aluno[2]}, Turma: {aluno[3]}")
@@ -87,10 +87,10 @@ def editar_aluno():
     "6": "3° EM Jogos Digitais"
 }
 
-    id_aluno = input("digite o ID do aluno que você quer editar:").strip()
+    id_aluno = input("digite o ID do aluno que você quer editar: \n").strip()
 
     if not id_aluno.isdigit():
-        print("ID deve ser um número válido")
+        print("ID deve ser um número válido \n")
 
     else: 
         cursor.execute("SELECT * FROM alunos WHERE id_aluno = %s", (id_aluno,))
@@ -99,26 +99,26 @@ def editar_aluno():
         if aluno:
             print(f"ID: {aluno[0]}, Nome: {aluno[1]}, Idade: {aluno[2]}, Turma: {aluno[3]}")
 
-            novo_nome = input("Digite o novo nome do aluno (reescreva o nome caso queira mante-lo):").strip()
+            novo_nome = input("Digite o novo nome do aluno (reescreva o nome caso queira mante-lo)\n:").strip()
 
-            nova_idade = input("digite a nova idade do aluno (redigite a idade caso queira mante-la):").strip()
+            nova_idade = input("digite a nova idade do aluno (redigite a idade caso queira mante-la)\n:").strip()
 
-            print("Opções de turma: \n 1: 1° EM DS \n 2: 1° EM multimídia \n 3: 1° EM Jogos Digitais \n 4: 2° EM Multimídia \n 5: 2° EM Jogos digitais \n 6: 3° EM Jogos Digitais")
+            print("Opções de turma: \n 1: 1° EM DS \n 2: 1° EM multimídia \n 3: 1° EM Jogos Digitais \n 4: 2° EM Multimídia \n 5: 2° EM Jogos digitais \n 6: 3° EM Jogos Digitais\n")
             
-            nova_turma = input("digite o numero da nova turma (redigite o número caso queira mante-lo):").strip()
+            nova_turma = input("digite o numero da nova turma (redigite o número caso queira mante-lo): \n").strip()
 
             if novo_nome == "" or nova_idade == "" or nova_turma == "":
-                print("Preencha todos os campos")
+                print("Preencha todos os campos\n")
 
             
             elif not novo_nome.replace(" ", "").isalpha():
-                print("Nome inválido")
+                print("Nome inválido\n")
 
             elif nova_turma not in ["1", "2", "3", "4", "5", "6"]:
                 print("Turma inválida")
             
             elif not nova_idade.isdigit() or int(nova_idade) <= 0:
-                print("Idade deve ser um número válido")
+                print("Idade deve ser um número válido\n")
 
             else:
 
@@ -128,11 +128,11 @@ def editar_aluno():
 
                 conexao.commit()
 
-                print("Aluno atualizado")
+                print("Aluno atualizado\n")
 
 
         else:
-            print("Aluno não encontrado")
+            print("Aluno não encontrado\n")
 
     cursor.close()
 
@@ -143,10 +143,10 @@ def excluir_aluno(): #ultima parte que eu venécios terei que fazer
 
     cursor = conexao.cursor()
 
-    id_aluno = input("digite o ID do aluno que você quer excluir:").strip()
+    id_aluno = input("digite o ID do aluno que você quer excluir: \n").strip()
 
     if not id_aluno.isdigit():
-        print("ID deve ser um número válido")
+        print("ID deve ser um número válido\n")
 
     else:
         cursor.execute("SELECT * FROM alunos WHERE id_aluno = %s", (id_aluno,))
@@ -159,10 +159,10 @@ def excluir_aluno(): #ultima parte que eu venécios terei que fazer
 
             conexao.commit()
 
-            print("Aluno excluído")
+            print("Aluno excluído\n")
 
         else:
-            print("Aluno não encontrado")
+            print("Aluno não encontrado\n")
 
     cursor.close()
 
