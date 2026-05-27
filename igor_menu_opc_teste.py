23db85aef19c4308c1ccfc8412e8ae26f0b84089
igor-junio <igorcarlosdasilvasilva3@gmail.com>
2026-05-26 21:39:08 -0300

    Update igor_menu_opc_teste.py

--- igor_menu_opc_teste.py
+++ igor_menu_opc_teste.py
@@ -272,7 +272,25 @@ if __name__ == "__main__":
         print("8. Verificar situação do aluno (Professor e Aluno)")
         print("9. Mostrar boletim completo (Professor e Aluno)")
         print("0. Sair")
+    calcular_media()
+    verificar_status()  
+    opcao = input("Digite o número da opção desejada: ").strip()
 
+ 
+    print("Opção inválida, tente novamente.\n")
+    meu_meu = "menu de administração de alunos e notas" \
+    "\n1. Cadastrar aluno (Administrador)" \
+    "\n2. Listar alunos (Administrador e Professor)" \
+    "\n3. Editar aluno (Administrador)" \
+    "\n4. Excluir aluno (Administrador)" \
+    "\n5. Adicionar nota (Professor)" \
+    "\n6. Remover nota (Professor)" \
+    "\n7. Calcular média (Aluno)" \
+    "\n8. Verificar situação do aluno (Professor e Aluno)" \
+    "\n9. Mostrar boletim completo (Professor e Aluno)" \
+    "\n0. Sair"
+    print(meu_meu)
+    while True:
         opcao = input("Digite o número da opção desejada: ").strip()
 
         if opcao == "1":
