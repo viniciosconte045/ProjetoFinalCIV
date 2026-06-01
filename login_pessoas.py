ed94ca1d893ce05ce7a7200c0a4d111a56cc848c
Vinicios Alves <viniciosconte045@gmail.com>
2026-06-01 14:33:39 -0300

    arrumando erros
    falta o my sql da parte da cecilia😫

--- login_pessoas.py
+++ login_pessoas.py
@@ -7,18 +7,6 @@ def conectar():
         password="Senac2026",
         database="escola_db"
     )
-
-
-import mysql.connector
-
-def conectar():
-    return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="Senac2026",
-        database="escola_db"
-    )
-
 def cadastrar_aluno():
     conexao = conectar()
 
