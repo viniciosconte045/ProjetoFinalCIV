25fd1454fba21fa738e21b9e0c8f4ed8ca14575b
igor-junio <igorcarlosdasilvasilva3@gmail.com>
2026-06-03 14:29:14 -0300

    Update igor_menu_opc_teste.py

--- igor_menu_opc_teste.py
+++ igor_menu_opc_teste.py
@@ -742,5 +742,18 @@ else:
             "Digite o número da opção desejada: " \
             "Opção inválida, tente novamente.\n"
             print(bolintni)
-             
+            menu_do_bolintni = "menu de administração de alunos e notas" \
+            "\n1. Cadastrar aluno (Administrador)" \
+            "\n2. Listar alunos (Administrador e Professor)"
+            "\n3. Editar aluno (Administrador)"
+            "\n4. Excluir aluno (Administrador)"
+            "\n5. Adicionar nota (Professor)"
+            "\n6. Remover nota (Professor)"
+            "\n7. Calcular média (Aluno)"
+            "\n8. Verificar situação do aluno (Professor e Aluno)"
+            "\n9. Mostrar boletim completo (Professor e Aluno)"
+            "\n0. Sair"
+            print(menu_do_bolintni)
+            
+
 # siquier mudar vique avontade o codico.
