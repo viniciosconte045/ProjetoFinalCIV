28090fb744a9cc983e9d84e35fa631a08789db09
Vinicios Alves <viniciosconte045@gmail.com>
2026-06-23 16:39:00 -0300

    falta terminar

--- banco.py
+++ banco.py
@@ -1,9 +1,74 @@
 import mysql.connector
 
 def conectar():
-    return mysql.connector.connect(
+    conexao = mysql.connector.connect(
         host="127.0.0.1",
         user="root",
         password="Senac2026",
         database="escola_db"
     )
+
+    cursor = conexao.cursor()
+
+    cursor.execute("CREATE DATABASE IF NOT EXISTS escola_db")
+    cursor.execute("USE escola_db")
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+        usuario VARCHAR(50) UNIQUE NOT NULL,
+        senha VARCHAR(100) NOT NULL,
+        tipo ENUM('admin','professor','aluno') NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS alunos (
+        id_aluno INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        idade INT NOT NULL,
+        turma VARCHAR(50) NOT NULL,
+        id_usuario INT,
+        FOREIGN KEY (id_usuario)
+        REFERENCES usuarios(id_usuario)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS professores (
+        id_professor INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        disciplina VARCHAR(100),
+        id_usuario INT,
+        FOREIGN KEY (id_usuario)
+        REFERENCES usuarios(id_usuario)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS notas (
+        id_nota INT AUTO_INCREMENT PRIMARY KEY,
+        id_aluno INT NOT NULL,
+        nota FLOAT NOT NULL,
+        FOREIGN KEY (id_aluno)
+        REFERENCES alunos(id_aluno)
+        ON DELETE CASCADE
+    )
+    """)
+
+    cursor.execute("""
+    INSERT IGNORE INTO usuarios (usuario, senha, tipo)
+    VALUES ('admin1', 'josefa', 'admin')
+    """)
+
+    cursor.execute("""
+    INSERT IGNORE INTO usuarios (usuario, senha, tipo)
+    VALUES ('professor', 'zion', 'professor')
+    """)
+
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    print("Banco de dados criado/verificado com sucesso!")
